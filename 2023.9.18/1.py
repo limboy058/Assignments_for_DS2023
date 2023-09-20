e0f584fb9bc56bf3134199f8c4e2bcd6eecacccb
limbo-t <2324899904@qq.com>
2023-09-20 11:30:35 +0800

    迁移到vscode

--- 2023.9.18/1.py
+++ 2023.9.18/1.py
@@ -1,18 +1,18 @@
-﻿n=int(input())
-if n==1:
-    print(1);
-elif n==2:
-    print(2);
+﻿n = int(input())
+if n == 1:
+    print(1)
+elif n == 2:
+    print(2)
 else:
-    if n%3==0:
-        for i in range(0,n//3):
-            print(3,end=" ")
+    if n % 3 == 0:
+        for i in range(0, n // 3):
+            print(3, end=" ")
         print()
-    elif n%3==1:
-        for i in range(0,(n-4)//3):
-            print(3,end=" ")
-        print(2,2)
+    elif n % 3 == 1:
+        for i in range(0, (n - 4) // 3):
+            print(3, end=" ")
+        print(2, 2)
     else:
-        for i in range(0,(n-2)//3):
-            print(3,end=" ")
+        for i in range(0, (n - 2) // 3):
+            print(3, end=" ")
         print(2)
